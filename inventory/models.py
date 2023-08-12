21e01c41e2c16edad5ce718c1753184afcdb3ff5
Daniel Elphinstone <daniel@elphonet.com>
2023-08-13 00:46:05 +1000

    initial commit

---
+++ inventory/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Inventory(models.Model):
+    name = models.CharField(max_length=300, null=False, blank=False)
+    item_code = models.CharField(max_length=300, null=False, blank=False)
+    cost_price = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
+    quantity_in_stock = models.IntegerField(null=False, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
---
+++ inventory/urls.py
@@ -0,0 +1,8 @@
+# inventory/urls.py
+
+from django.urls import path
+from .views import index
+
+urlpatterns = [
+    path("", index, name="index")
+]
