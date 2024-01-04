c674eae700b9322772f22f4f842b0f2263d84032
Daniel Elphinstone <daniel@elphonet.com>
2024-01-05 10:26:53 +1100

    Added barcode to DB model

--- inventory/models.py
+++ inventory/models.py
@@ -7,10 +7,12 @@ class Inventory(models.Model):
     brand = models.CharField(max_length=300, null=False, blank=False)
     title = models.CharField(max_length=300, null=False, blank=False)
     manufacturer_part_number = models.CharField(max_length=300, null=False, blank=False)
-    cost_price = models.DecimalField(max_digits=19, decimal_places=2, null=False, blank=False)
+    barcode = models.CharField(max_length=300, null=False, blank=False)
+    cost_price = models.DecimalField(
+        max_digits=19, decimal_places=2, null=False, blank=False
+    )
     quantity_in_stock = models.IntegerField(null=False, blank=False)
     description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
-
